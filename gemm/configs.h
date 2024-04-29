60c9e8c6ebe8b268d52a67406196e0ed14907fc8
Fridge003 <eddiezhang@pku.edu.cn>
2024-04-29 23:56:29 +0800

    modify config

--- gemm/configs.h
+++ gemm/configs.h
@@ -4,16 +4,16 @@
 #include <string>
 
 // List of m, n and k values that are supposed to be tested.
-const std::vector<int> m_list = {128, 256, 512, 1024, 2048};
-const std::vector<int> n_list = {128, 256, 512, 1024, 2048};
-const std::vector<int> k_list = {128, 256, 512, 1024, 2048};
+const std::vector<int> m_list = {128, 256, 512, 1024, 2048, 4096};
+const std::vector<int> n_list = {128, 256, 512, 1024, 2048, 4096};
+const std::vector<int> k_list = {128, 256, 512, 1024, 2048, 4096};
 
 // const std::vector<int> m_list = {2, 8, 32, 128, 2, 8, 32, 128};
 // const std::vector<int> n_list = {2048, 2048, 2048, 2048, 4096, 4096, 4096, 4096};
 // const std::vector<int> k_list = {2048, 2048, 2048, 2048, 4096, 4096, 4096, 4096};
 
 // List of kernels that have been implemented and are supposed to be tested.
-const std::vector<std::string> kernel_list = {"naive", "cublas", "global_memory_coalescing"};
+const std::vector<std::string> kernel_list = {"naive", "global_memory_coalescing", "cublas"};
 
 // Parameters of V100 for PCIe
 // const double device_fp32_compute_capacity_tflops = 14.0;
